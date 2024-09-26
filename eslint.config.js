610638adba696cce4025a36d3816d4f3cdaa6bcf
ksenia-b <ksenia.barshchyk@gmail.com>
2024-09-26 12:03:31 +0300

    setted up eslisnt

---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"]
+  },
+  {
+    languageOptions: { globals: globals.browser }
+  },
+  {
+    rules: {
+      "no-unused-vars": "warn",
+      "no-undef": "warn"
+    }
+  },
+  pluginJs.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
+
+import js from "@eslint/js";
+
